f03a951ee5ba6ed87da657f0e9606a6c0645c8a4
Juan Daniel Flores <juandfloresm@gmail.com>
2026-07-01 20:34:04 +0200

    New user service template

---
+++ microservices/user-service/src/test/java/es/flores/microservices/core/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.flores.microservices.core.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
